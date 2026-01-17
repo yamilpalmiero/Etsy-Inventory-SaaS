3f5a49ff0050c924f8094f53eb325e7eb31a6c00
Yamil <yamilpalmiero@gmail.com>
2026-01-17 17:10:55 +0100

    feat: complete Etsy OAuth flow with store management

--- stores/urls.py
+++ stores/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'stores'
+
 urlpatterns = [
-    path('connect/', views.etsy_auth_init, name='etsy_auth_init'),
-    path('callback/', views.etsy_auth_callback, name='etsy_auth_callback'),
-    path('list/', views.store_list, name='store_list'),
-    path('<int:store_id>/disconnect/', views.store_disconnect, name='store_disconnect'),
+    path('auth/etsy/init/', views.etsy_auth_init, name='etsy_auth_init'),
+    path('auth/etsy/callback/', views.etsy_auth_callback, name='etsy_auth_callback'),
+    path('stores/', views.store_list, name='store_list'),
+    path('stores/<int:store_id>/disconnect/', views.store_disconnect, name='store_disconnect'),
 ]
